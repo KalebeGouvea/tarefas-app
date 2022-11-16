6f3706aa3422c017c7a546eff78dd90b6396a7a5
Kalebe <k.gouvea@live.com>
2022-11-16 11:31:31 -0300

    Add injecao de dependencia no controller

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using tarefas_app.Context;
 
 namespace tarefas_app.Controllers
 {
     public class TarefaController : Controller
     {
+        private readonly TarefaContext _context;
+
+        public TarefaController(TarefaContext context)
+        {
+            _context = context;
+        }
+        
         public IActionResult Index()
         {
             return View();
