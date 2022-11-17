497b84cc0da4f44783f2e96a8eecae7f1d2492c9
Kalebe <k.gouvea@live.com>
2022-11-17 15:50:43 -0300

    Implementada funcionalidade Deletar, alterados view e controller

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -73,5 +73,25 @@ namespace tarefas_app.Controllers
             
             return View(tarefa);
         }
+
+        public IActionResult Deletar(int id)
+        {
+            var tarefa = _context.Tarefas.Find(id);
+
+            if (tarefa == null)
+                return RedirectToAction(nameof(Index));
+            
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(Tarefa tarefa)
+        {
+            var tarefaBanco = _context.Tarefas.Find(tarefa.Id);
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
