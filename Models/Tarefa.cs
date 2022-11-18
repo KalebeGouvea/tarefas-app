7d6705d80b5d984c33314a935d90b9233a4852a6
Kalebe <k.gouvea@live.com>
2022-11-18 15:51:22 -0300

    Add DataAnnotations nos campos do Model

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace tarefas_app.Models
 {
@@ -9,7 +10,12 @@ namespace tarefas_app.Models
     {
         public int Id { get; set; }
         public string Titulo { get; set; }
+
+        [Display(Name = "Descrição")]
         public string? Descricao { get; set; }
+
+        [Display(Name = "Data de conclusão")]
+        [DataType(DataType.Date)]
         public DateTime Data { get; set; }
         public bool Status { get; set; }
     }
